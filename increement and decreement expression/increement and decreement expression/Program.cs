eef63f9aafc4ae749141079dfbcb854b2c2c7a85
yassienh <eng_monaliza11@yahoo.com>
2017-05-28 16:59:03 +0300

    icrement and decreement expression
    explain how we use increment and decrement expression in c#

---
+++ increement and decreement expression/increement and decreement expression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace increement_and_decreement_expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvar1 = 7;
+            int myvar2 = 2;
+            //int temp = myvar1-- / ++myvar2;
+            // myvar1 = myvar2;
+            myvar1 += myvar2; //myvar1=myvar1+myvar2
+            myvar1 -=myvar2;
+            myvar1 /= myvar2;
+            myvar1 %= myvar2;
+
+            // Console.WriteLine("{0}", temp);
+            Console.WriteLine("{0}",myvar1);
+            Console.ReadKey();
+        }
+    }
+}
